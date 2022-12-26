ec93cd307eb9c60de6474f8b3f31298215911383
Waleed AlMalki <wm.wexem@gmail.com>
2022-12-26 16:22:43 +0100

    Added Limit modifiers

---
+++ limit.go
@@ -0,0 +1,60 @@
+package iterator
+
+// LimitFunc returns a modifier that stops the iterator when fn
+// returns false or an error
+func LimitFunc[T any](fn func(uint64, T) (bool, error)) Modifier[T, T] {
+	return func(iter Iterator[T]) Iterator[T] {
+		var count uint64
+		var finished bool
+		var err error
+
+		return &iterator[T]{
+			next: func() bool {
+				if err != nil {
+					return false
+				}
+
+				if finished {
+					return false
+				}
+				if !iter.Next() {
+					finished = true
+					return false
+				}
+
+				var item T
+				item, err = iter.Get()
+				if err != nil {
+					finished = true
+					return false
+				}
+
+				var ok bool
+				ok, err = fn(count, item)
+				if !ok || err != nil {
+					finished = true
+					return false
+				}
+
+				count++
+				return true
+			},
+			get:   iter.Get,
+			close: iter.Close,
+			err: func() error {
+				if err != nil {
+					return err
+				}
+				return iter.Err()
+			},
+		}
+	}
+}
+
+// Limit returns a modifier that stops the iterator when it
+// has progressed times equal to limit
+func Limit[T any](limit uint64) Modifier[T, T] {
+	return LimitFunc(func(count uint64, _ T) (bool, error) {
+		return count < limit, nil
+	})
+}
