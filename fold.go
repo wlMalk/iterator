411c9e23e196c348641f8ca716684f758c0bd2fc
Waleed AlMalki <wm.wexem@gmail.com>
2022-12-27 23:13:34 +0100

    Added Fold operations

---
+++ fold.go
@@ -0,0 +1,63 @@
+package iterator
+
+// Fold all items into a single value with a start value by applying fn on all items
+func Fold[T any, S any](iter Iterator[T], start S, fn func(uint, T, S) (S, error)) (S, error) {
+	reduced := start
+
+	_, err := Iterate(iter, func(i uint, value T) (bool, error) {
+		var err error
+		reduced, err = fn(i, value, reduced)
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	})
+	if err != nil {
+		return *new(S), err
+	}
+
+	return reduced, nil
+}
+
+// Reduce all items into a single value by applying fn on all items
+func Reduce[T any](iter Iterator[T], fn func(uint, T, T) (T, error)) (T, error) {
+	if !iter.Next() {
+		iter.Close()
+		return *new(T), ErrNoItems
+	}
+
+	first, err := iter.Get()
+	if err != nil {
+		iter.Close()
+		return *new(T), err
+	}
+
+	return Fold(iter, first, fn)
+}
+
+// Sum all numbers in the iterator
+func Sum[T Number](iter Iterator[T]) (T, error) {
+	return Fold(iter, 0, func(_ uint, item T, total T) (T, error) {
+		return total + item, nil
+	})
+}
+
+// Concat all items with the given separator
+func Concat[T ~string](iter Iterator[T], sep T) (T, error) {
+	res, err := Fold(iter, sep, func(i uint, item T, total T) (T, error) {
+		if i == 0 {
+			return total + item, nil
+		}
+		return total + sep + item, nil
+	})
+	if err != nil {
+		return *new(T), err
+	}
+
+	return res[len(sep):], nil
+}
+
+// String converts and concatenates all items into a single string
+func String[T any](iter Iterator[T]) (string, error) {
+	return Concat(Strings(iter), "")
+}
---
+++ fold_test.go
@@ -0,0 +1,62 @@
+package iterator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReduce(t *testing.T) {
+	cases := []struct {
+		iter     Iterator[int]
+		expected int
+		err      error
+	}{
+		{Empty[int](), 0, ErrNoItems},
+		{Once(1), 1, nil},
+		{Limit[int](5)(Ascending(0, 1)), 10, nil},
+	}
+
+	for i := range cases {
+		sum, err := Reduce(cases[i].iter, func(_ uint, item, sum int) (int, error) {
+			return sum + item, nil
+		})
+		require.ErrorIs(t, err, cases[i].err)
+		assert.Equal(t, cases[i].expected, sum)
+	}
+}
+
+func TestSum(t *testing.T) {
+	cases := []struct {
+		iter     Iterator[int]
+		expected int
+	}{
+		{Empty[int](), 0},
+		{Once(1), 1},
+		{Limit[int](5)(Ascending(0, 1)), 10},
+	}
+
+	for i := range cases {
+		sum, err := Sum(cases[i].iter)
+		require.NoError(t, err)
+		assert.Equal(t, cases[i].expected, sum)
+	}
+}
+
+func TestString(t *testing.T) {
+	cases := []struct {
+		iter     Iterator[int]
+		expected string
+	}{
+		{Empty[int](), ""},
+		{Once(1), "1"},
+		{Limit[int](5)(Ascending(0, 1)), "01234"},
+	}
+
+	for i := range cases {
+		str, err := String(cases[i].iter)
+		require.NoError(t, err)
+		assert.Equal(t, cases[i].expected, str)
+	}
+}
