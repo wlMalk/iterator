68135df022347a647a3932029e3fb694a04bebe7
Waleed AlMalki <wm.wexem@gmail.com>
2023-01-09 20:51:16 +0100

    Modified JSON tests

--- json/json_test.go
+++ json/json_test.go
@@ -4,27 +4,27 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/wlMalk/iterator"
+	it "github.com/wlMalk/iterator"
 	"github.com/wlMalk/iterator/internal/utils"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func checkIteratorEqual[T comparable](t *testing.T, iter iterator.Iterator[T], items []T) {
+func checkIteratorEqual[T comparable](t *testing.T, iter it.Iterator[T], items []T) {
 	utils.CheckIteratorEqual[T](t, iter, items)
 }
 
 func TestIteratorMarshalJSON(t *testing.T) {
-	it := Iterator[int]{iterator.Range(1, 5, 1)}
-	b, err := json.Marshal(it)
+	iter := Iterator[int]{it.Range(1, 5, 1)}
+	b, err := json.Marshal(iter)
 	require.NoError(t, err)
 	assert.Equal(t, []byte("[1,2,3,4,5]"), b)
 }
 
 func TestIteratorUnmarshalJSON(t *testing.T) {
-	var it Iterator[int]
-	err := json.Unmarshal([]byte("[1,2,3,4,5]"), &it)
+	var iter *Iterator[int]
+	err := json.Unmarshal([]byte("[1,2,3,4,5]"), &iter)
 	require.NoError(t, err)
-	checkIteratorEqual[int](t, it, []int{1, 2, 3, 4, 5})
+	checkIteratorEqual[int](t, iter, []int{1, 2, 3, 4, 5})
 }
