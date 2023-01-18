a9da720e402781702c24ee89f50185e8c6644685
Waleed AlMalki <wm.wexem@gmail.com>
2023-01-18 23:44:28 +0100

    Added comments

--- contains.go
+++ contains.go
@@ -1,5 +1,7 @@
 package iterator
 
+// ContainsFunc calls fn on each item in the given iterator.
+// It stops as soon as fn returns true and reports that.
 func ContainsFunc[T any](iter Iterator[T], fn func(uint, T) (bool, error)) (bool, error) {
 	result := false
 	if _, err := Iterate(iter, func(i uint, item T) (bool, error) {
@@ -17,6 +19,7 @@ func ContainsFunc[T any](iter Iterator[T], fn func(uint, T) (bool, error)) (bool
 	return result, nil
 }
 
+// ContainsAny returns true as soon as it encounters any of the given values in the given iterator.
 func ContainsAny[T comparable](iter Iterator[T], values ...T) (bool, error) {
 	return ContainsFunc(iter, func(_ uint, item T) (bool, error) {
 		for _, value := range values {
@@ -28,6 +31,7 @@ func ContainsAny[T comparable](iter Iterator[T], values ...T) (bool, error) {
 	})
 }
 
+// ContainsAll returns true when it has encountered all the given values in the given iterator.
 func ContainsAll[T comparable](iter Iterator[T], values ...T) (bool, error) {
 	return ContainsFunc(iter, func(_ uint, item T) (bool, error) {
 		for i, value := range values {
