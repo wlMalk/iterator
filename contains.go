32118f3a4ca0f26fbc89f04d5570fd912ba6752b
Waleed AlMalki <wm.wexem@gmail.com>
2023-01-18 23:39:39 +0100

    Added Contains operations

---
+++ contains.go
@@ -0,0 +1,44 @@
+package iterator
+
+func ContainsFunc[T any](iter Iterator[T], fn func(uint, T) (bool, error)) (bool, error) {
+	result := false
+	if _, err := Iterate(iter, func(i uint, item T) (bool, error) {
+		contains, err := fn(i, item)
+		if err != nil {
+			return false, err
+		}
+		if contains {
+			result = true
+		}
+		return !contains, nil
+	}); err != nil {
+		return false, err
+	}
+	return result, nil
+}
+
+func ContainsAny[T comparable](iter Iterator[T], values ...T) (bool, error) {
+	return ContainsFunc(iter, func(_ uint, item T) (bool, error) {
+		for _, value := range values {
+			if item == value {
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+}
+
+func ContainsAll[T comparable](iter Iterator[T], values ...T) (bool, error) {
+	return ContainsFunc(iter, func(_ uint, item T) (bool, error) {
+		for i, value := range values {
+			if item == value {
+				if len(values) == 1 {
+					return true, nil
+				}
+				values = append(values[:i], values[i+1:]...)
+				return false, nil
+			}
+		}
+		return false, nil
+	})
+}
