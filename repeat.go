4e3071bbea4c87c7140af558e1b1a8146ef2dc80
Waleed AlMalki <wm.wexem@gmail.com>
2023-01-29 21:08:56 +0100

    Added Repeat modifier

--- repeat.go
+++ repeat.go
@@ -1,11 +1,73 @@
 package iterator
 
+// Repeat the items in the range [from, to) for as many times given.
+// All other items outside this range are included in the iterator as well.
+func Repeat[T any](from uint, to uint, times int) Modifier[T, T] {
+	return func(iter Iterator[T]) Iterator[T] {
+		var count uint
+		if from > to {
+			from, to = to, from
+		}
+		items := make([]T, 0, to-from)
+		var curr, currTime = -1, 0
+		var isRepeating bool
+		return OnClose(FromFunc(func() (T, bool, error) {
+			var hasMore bool
+			var advanced bool
+			if !isRepeating && count >= from && count < to {
+				hasMore = iter.Next()
+				advanced = true
+				if !hasMore {
+					isRepeating = true
+				}
+			}
+
+			if isRepeating && len(items) > 0 {
+				curr++
+				if curr == len(items) {
+					curr = 0
+					currTime++
+				}
+				if currTime < times {
+					return items[curr], true, nil
+				}
+			}
+			isRepeating = false
+
+			if (!advanced && iter.Next()) || (advanced && hasMore) {
+				item, err := iter.Get()
+				if err != nil {
+					return *new(T), false, err
+				}
+				if count >= from && count < to {
+					items = append(items, item)
+				}
+				count++
+				if count == to {
+					isRepeating = true
+				}
+				return item, true, nil
+			}
+
+			if err := iter.Err(); err != nil {
+				return *new(T), false, err
+			}
+
+			if err := iter.Close(); err != nil {
+				return *new(T), false, err
+			}
+
+			return *new(T), false, nil
+		}), iter.Close)
+	}
+}
+
 func cycle[T any](times int, reversed bool) Modifier[T, T] {
 	return func(iter Iterator[T]) Iterator[T] {
 		var items []T
-		var curr, currTime = -1, -1
+		var curr, currTime = -1, 0
 		var iterFinished bool
-		return FromFunc(func() (T, bool, error) {
+		return OnClose(FromFunc(func() (T, bool, error) {
 			if !iterFinished {
 				if iter.Next() {
 					item, err := iter.Get()
@@ -16,6 +78,9 @@ func cycle[T any](times int, reversed bool) Modifier[T, T] {
 					return item, true, nil
 				}
 				if err := iter.Err(); err != nil {
+					return *new(T), false, err
+				}
+				if err := iter.Close(); err != nil {
 					return *new(T), false, err
 				}
 				iterFinished = true
@@ -59,7 +124,7 @@ func cycle[T any](times int, reversed bool) Modifier[T, T] {
 			}
 
 			return items[curr], true, nil
-		})
+		}), iter.Close)
 	}
 }
 
--- repeat_test.go
+++ repeat_test.go
@@ -4,6 +4,33 @@ import (
 	"testing"
 )
 
+func TestRepeat(t *testing.T) {
+	cases := []struct {
+		iter     Iterator[int]
+		from, to uint
+		times    int
+		expected []int
+	}{
+		{Range(1, 4, 1), 0, 1, 2, []int{1, 1, 1, 2, 3, 4}},
+		{Range(1, 4, 1), 3, 4, 2, []int{1, 2, 3, 4, 4, 4}},
+		{Range(1, 4, 1), 1, 2, 1, []int{1, 2, 2, 3, 4}},
+		{Range(1, 4, 1), 1, 2, 2, []int{1, 2, 2, 2, 3, 4}},
+		{Range(1, 4, 1), 1, 3, 2, []int{1, 2, 3, 2, 3, 2, 3, 4}},
+		{Range(1, 4, 1), 0, 4, 1, []int{1, 2, 3, 4, 1, 2, 3, 4}},
+		{Range(1, 4, 1), 1, 1, 2, []int{1, 2, 3, 4}},
+		{Range(1, 4, 1), 0, 1, 0, []int{1, 2, 3, 4}},
+		{Range(1, 4, 1), 1, 0, 1, []int{1, 1, 2, 3, 4}},
+		{Range(1, 4, 1), 0, 10, 2, []int{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4}},
+		{Once(1), 0, 1, 1, []int{1, 1}},
+		{Once(1), 0, 10, 1, []int{1, 1}},
+		{Empty[int](), 0, 1, 1, []int{}},
+	}
+
+	for i := range cases {
+		checkIteratorEqual(t, Repeat[int](cases[i].from, cases[i].to, cases[i].times)(cases[i].iter), cases[i].expected)
+	}
+}
+
 func TestCycle(t *testing.T) {
 	cases := []struct {
 		iter     Iterator[int]
